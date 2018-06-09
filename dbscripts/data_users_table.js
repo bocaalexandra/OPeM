93320aa0ee7d6fc70f1057cf2ab23bf68186fd2d
Alexandra <boca_alexandra96@yahoo.com>
2018-06-09 16:38:39 +0300

    Add files via upload

---
+++ dbscripts/data_users_table.js
@@ -0,0 +1,41 @@
+var mysql = require('mysql');
+var created = true;
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database:"mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  if(created == false)
+  	{
+  		con.query("CREATE DATABASE mydb", function (err, result) {
+    	if (err) throw err;
+    	console.log("Database created");
+    	});
+    	created = true;
+    }
+  var sql = "CREATE TABLE users_data (ID int PRIMARY KEY NOT NULL AUTO_INCREMENT, FirstName VARCHAR(255), LastName VARCHAR(255), Email VARCHAR(255), DateOfBirth DATE, PhoneNumber VARCHAR(10))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+
+   con.query("INSERT INTO users_data (FirstName,LastName,Email,DateOfBirth,PhoneNumber) values ('Popescu','Ion','popescuion@gmail.com','1997-08-10','0775671324')", function (err, result, fields) {
+   if (err) throw err;
+   console.log(result);
+   });
+   con.query("INSERT INTO users_data (FirstName,LastName,Email,DateOfBirth,PhoneNumber) values ('Breazu','Maria','breazumaria@gmail.com','2002-03-22','0775582324')", function (err, result, fields) {
+   if (err) throw err;
+   console.log(result);
+   });
+   con.query("INSERT INTO users_data (FirstName,LastName,Email,DateOfBirth,PhoneNumber) values ('Poiana','Matei','poianamatei@gmail.com','1989-11-18','0748982356')", function (err, result, fields) {
+   if (err) throw err;
+   console.log(result);
+   });
+  
+ });
