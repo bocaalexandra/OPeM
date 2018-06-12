d43437d970ddb5f8428d6209c635aa506fa79f66
Alexandra <boca_alexandra96@yahoo.com>
2018-06-12 22:44:43 +0300

    Add files via upload

--- dbscripts/users.js
+++ dbscripts/users.js
@@ -19,24 +19,30 @@ con.connect(function(err) {
     	});
     	created = true;
     }
-
-  var sql = "CREATE TABLE users (UserID int, FOREIGN KEY (UserID) REFERENCES users_data(ID), Email VARCHAR(50), Password VARCHAR(50))";
+  
+  var sql = "ALTER TABLE users ADD UNIQUE (Email)";
   con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Table created");
-  });
+     if (err) throw err;
+     console.log("Table altered!");
+   });
 
-  con.query("INSERT INTO users (UserID,Email,Password) values (1,'popescuion@gmail.com','1234567')", function (err, result, fields) {
-   if (err) throw err;
-   console.log(result);
-   });
-   con.query("INSERT INTO users (UserID,Email,Password) values (2,'breazumaria@gmail.com','iammaria')", function (err, result, fields) {
-   if (err) throw err;
-   console.log(result);
-    });
-   con.query("INSERT INTO users (UserID,Email,Password) values (3,'poianamatei@gmail.com','thisismypass')", function (err, result, fields) {
-   if (err) throw err;
-   console.log(result);
-   });
+  // var sql = "CREATE TABLE users (UserID int, FOREIGN KEY (UserID) REFERENCES users_data(ID) ON DELETE CASCADE, Email VARCHAR(50), Password VARCHAR(50))";
+  // con.query(sql, function (err, result) {
+  //   if (err) throw err;
+  //   console.log("Table created");
+  // });
+
+  // con.query("INSERT INTO users (UserID,Email,Password) values (1,'popescuion@gmail.com','1234567')", function (err, result, fields) {
+  //  if (err) throw err;
+  //  console.log(result);
+  //  });
+  //  con.query("INSERT INTO users (UserID,Email,Password) values (2,'breazumaria@gmail.com','iammaria')", function (err, result, fields) {
+  //  if (err) throw err;
+  //  console.log(result);
+  //   });
+  //  con.query("INSERT INTO users (UserID,Email,Password) values (3,'poianamatei@gmail.com','thisismypass')", function (err, result, fields) {
+  //  if (err) throw err;
+  //  console.log(result);
+  //  });
 
 });
